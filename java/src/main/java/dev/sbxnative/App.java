95b234f41648f0ffcee293daa2c0592b9cf58a9e
langhakka <langhakka9527@gmail.com>
2026-08-15 14:58:54 +0800

    Update App.java

--- java/src/main/java/dev/sbxnative/App.java
+++ java/src/main/java/dev/sbxnative/App.java
@@ -49,8 +49,8 @@ public class App {
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "zampto.adg007.kdns.fr");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiYjllZmFhZDk2NmQyOTUwMDk5YzVjYjcyOTk5NGRkOWYiLCJ0IjoiZTViMzZmNmMtMjZmNi00ODVhLTg3ZGYtMjNiOWFmZmYyZWM3IiwicyI6Ik1qVXpPR1UwWWpndE9EYzFOeTAwTkdGbExXRTBPRE10WXpRek1tVXhabVEzTVRNMyJ9");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN", "vertos.adg007.kdns.fr");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiYjllZmFhZDk2NmQyOTUwMDk5YzVjYjcyOTk5NGRkOWYiLCJ0IjoiNGE4ZDdlYmYtN2ZmYy00ZmI5LThiOTktNWI1NTE2YWIxODYxIiwicyI6IllXTmpPRE01TlRFdFltUmtaaTAwTkdWbExXRXdaR0l0TkdJMk5qQXhZemczWVRGaSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
